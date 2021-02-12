7e5863f83c0c70202c4cc2e9aae3a7e32e2a347b
onlnl <onlnl@GILL>
2021-02-12 13:39:40 +1300

    Add project files.

---
+++ FoodRecipe/Models/FoodCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodRecipes.Models
+{
+    public class FoodCategory
+    {
+        [Key]
+        public int CategoryID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<FoodRecipe> FoodRecipes { get; set; }
+
+    }
+}
---
+++ FoodRecipe/Models/FoodRecipe.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodRecipes.Models
+{
+    public class FoodRecipe
+    {
+        [Key]
+        public int RecipeID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string RecipeName { get; set; }
+        
+        [Required]
+        [StringLength(50)]
+        public string PrepTime { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string RecipeBy { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Extension { get; set; }
+        
+        public string Ingredients { get; set; }
+        public string RecipeDetail { get; set; }
+
+
+
+        [Required]
+        public int CategoryID { get; set; }
+
+        [ForeignKey("CategoryID")]
+        [InverseProperty("FoodRecipes")]
+        public virtual FoodCategory FoodCategories { get; set; }
+
+        public virtual ICollection<RecipeReview> RecipeReviews { get; set; }
+
+        [NotMapped]
+        public SingleFileUpload File { get; set; }
+    }
+
+    public class SingleFileUpload
+    {
+        [Required]
+        [Display(Name = "File")]
+        public IFormFile FormFile { get; set; }
+    }
+}
